5def720ea8ea10d8d163ecf4f98dbf4a9fa075b8
Jonathan Pastor <jonathancmoa@gmail.com>
2016-06-15 16:23:20 -0500

    Update gpu_spec.rb

--- lib/g5kchecks/spec/gpu/gpu_spec.rb
+++ lib/g5kchecks/spec/gpu/gpu_spec.rb
@@ -1,49 +1,39 @@
 describe "Gpu" do
 
-  before(:all) do
-    @api_desc = RSpec.configuration.node.api_description["gpus"]
-    @api = {}
-    if @api_desc
-      @api_desc.each { |a|
-        @api[a['name']] = a
-      }
-    end
+  @api = RSpec.configuration.node.api_description['gpu']
+  @system = RSpec.configuration.node.ohai_description['gpu']
+
+  puts @api
+  # Skip remaining tests if there are no gpus installed on the node
+  it "should have the correct gpu boolean value" do
+    vendor_api = ""
+    vendor_api = @api['gpu'] if @api
+    vendor_ohai =  @system['gpu']
+    vendor_ohai.should eql(vendor_api), "#{vendor_ohai}, #{vendor_api}, gpu, #{name}, gpu"
   end
 
-  RSpec.configuration.node.ohai_description[:gpu][:gpus].each do |name, dev|
 
-    it "should have the correct gpu boolean value" do
-      vendor_api = ""
-      vendor_api = @api[dev[0]]['gpu'] if @api_desc
-      vendor_ohai = dev[:gpu]
-      vendor_ohai.should eql(vendor_api), "#{vendor_ohai}, #{vendor_api}, gpu, #{name}, gpu"
-    end
-
-    # Skip remaining tests if there are no gpus installed on the node
-    if not dev[:gpu]
-      next
-    end
-
+  if @api['gpu']
     it "should have the correct gpu_count" do
       vendor_api = ""
-      vendor_api = @api[dev[0]]['gpu_count'] if @api_desc
-      vendor_ohai = dev[:gpu_count]
+      vendor_api = @api['gpu_count'] if @api
+      vendor_ohai = @system['gpu_count']
       vendor_ohai.should eql(vendor_api), "#{vendor_ohai}, #{vendor_api}, gpu, #{name}, gpu_count"
     end
 
     it "should have the correct gpu_model" do
       model_api = ""
-      model_api = @api[dev[0]]['gpu_model'] if @api_desc
-      model_ohai = dev[:gpu_model]
+      model_api = @api['gpu_model'] if @api
+      model_ohai = @system['gpu_model']
       model_ohai.should eql(model_api), "#{model_ohai}, #{model_api}, gpu, #{name}, gpu_model"
     end
 
     it "should have the correct gpu_vendor" do
       name_api = ""
-      name_api = @api[dev[0]]['gpu_vendor'] if @api_desc
-      name_ohai = dev[:gpu_vendor]
+      name_api = @api['gpu_vendor'] if @api
+      name_ohai = @system['gpu_vendor']
       name_ohai.should eql(name_api), "#{name_ohai}, #{name_api}, gpu, #{name}, gpu_vendor"
     end
-
   end
+
 end
