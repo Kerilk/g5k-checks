b9afc094090bfa98516308c2f36d60ff3e56d5d4
badock <jonathan.pastor@me.com>
2016-11-14 21:48:40 +0000

    compute an accurate value for the frequency;

--- lib/g5kchecks/ohai/cpu.rb
+++ lib/g5kchecks/ohai/cpu.rb
@@ -42,23 +42,8 @@ else
   end
 end
 
-if File.exist?("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")
-  file = File.open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", "r")
-  freq = file.read
-  file.close
-  # frequence en khz
-  cpu[:mhz] = (freq.to_i)*1000 if freq
-else
-  cpu[:mhz] = cpu[:'0'][:mhz].to_i*1000000
-end
-
-cpu[:mhz] = (cpu[:mhz].to_f/1000000000)
-if  cpu[:vendor] == "Intel"
-    cpu[:mhz] = sprintf("%.2f", cpu[:mhz]).to_f
-else
-    cpu[:mhz] = sprintf("%.1f", cpu[:mhz]).to_f
-end
-cpu[:mhz] = (cpu[:mhz]*1000000000).to_i
+frequency_megahertz = execute("sudo dmidecode | grep \"Current Speed:\" | grep -oE \"[0-9]+\" | head -n 1").to_i
+cpu[:mhz] = frequency_megahertz * 1000000000
 
 popen4("lscpu") do |pid, stdin, stdout, stderr|
   stdin.close
@@ -78,9 +63,6 @@ popen4("lscpu") do |pid, stdin, stdout, stderr|
   end
 end
 
-#cpu[:extra] = 'test'
-
-# 
 cpu[:clock_speed] = (execute('x86info').last.split(" ").last.to_f * 1000 * 1000).to_i rescue 'unknown'
 
 # Parsing 'lscpu -p' output to retrieve :nb_procs, :nb_cores and :nb_threads
