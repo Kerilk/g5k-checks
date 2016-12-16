75042d860963d97218afc446be8e13e1b1424f8b
Pierre Riteau <priteau@uchicago.edu>
2016-12-16 16:36:10 +0000

    Build script changes

--- build_script/build_rpm.sh
+++ build_script/build_rpm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 set -eu
 
 ############################################################
@@ -54,7 +53,6 @@ find $BUILD_FOLDER -name "*.rpm"
 RPM_CANDIDATE_FILE=$(find /home/cc/rpmbuild -name '*.rpm' | grep -v "SRPMS" | head -n 1)
 echo ""
 echo "To install the RPM, use the following command:"
-echo "  sudo yum localinstall $RPM_CANDIDATE_FILE"
-
+echo "sudo yum localinstall -y $RPM_CANDIDATE_FILE"
 
 exit 0
