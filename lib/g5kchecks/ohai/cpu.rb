24509f97c7fbbc4fc97dcb7fc584fc9d26562846
Pierre Riteau <priteau@uchicago.edu>
2016-12-16 15:15:37 +0000

    Remove unused local change

--- lib/g5kchecks/ohai/cpu.rb
+++ lib/g5kchecks/ohai/cpu.rb
@@ -42,8 +42,23 @@ else
   end
 end
 
-# frequency_megahertz = execute("sudo dmidecode | grep \"Current Speed:\" | grep -oE \"[0-9]+\" | head -n 1").to_i
-# cpu[:mhz] = frequency_megahertz * 1000000000
+if File.exist?("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")
+  file = File.open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", "r")
+  freq = file.read
+  file.close
+  # frequence en khz
+  cpu[:mhz] = (freq.to_i)*1000 if freq
+else
+  cpu[:mhz] = cpu[:'0'][:mhz].to_i*1000000
+end
+
+cpu[:mhz] = (cpu[:mhz].to_f/1000000000)
+if  cpu[:vendor] == "Intel"
+    cpu[:mhz] = sprintf("%.2f", cpu[:mhz]).to_f
+else
+    cpu[:mhz] = sprintf("%.1f", cpu[:mhz]).to_f
+end
+cpu[:mhz] = (cpu[:mhz]*1000000000).to_i
 
 popen4("lscpu") do |pid, stdin, stdout, stderr|
   stdin.close
