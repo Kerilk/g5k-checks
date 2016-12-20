b511b8cd2d038127bc23bbfb27c30a72731fbcfd
Pierre Riteau <priteau@uchicago.edu>
2016-12-20 15:19:00 +0000

    Continue supporting old architecture keys

--- lib/g5kchecks/spec/architecture/architecture_spec.rb
+++ lib/g5kchecks/spec/architecture/architecture_spec.rb
@@ -13,6 +13,25 @@ describe "Architecture" do
   end
 
   [:nb_procs, :nb_cores, :nb_threads].each { |key|
+
+    # /!\ Warning: As some names of CPUs properties have changed in the
+    # grid'5000 api: the following block ensure compatibility between
+    # Chameleon and grid'5000 api
+    old_api_key = nil
+    if key == :nb_threads
+      old_api_key = :smt_size
+    elsif key == :nb_procs
+      old_api_key = :smp_size
+    end
+
+    if old_api_key
+      it "should have the correct value for #{old_api_key}" do
+        key_ohai = @system[:cpu][key]
+        key_api = @api[old_api_key.to_s] if @api
+
+        key_ohai.should eq(key_api), "#{key_ohai}, #{key_api}, architecture, #{old_api_key}"
+      end
+    end
     
     it "should have the correct value for #{key}" do
       key_ohai = @system[:cpu][key]
