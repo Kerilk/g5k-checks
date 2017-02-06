195b6d7c315d61cac7a4850ff0d7ab5b1d5e679f
Pierre Riteau <priteau@uchicago.edu>
2017-02-06 11:04:51 +0000

    Make DMI plugin compatible with Ohai 8

--- lib/g5kchecks/ohai/dmi.rb
+++ lib/g5kchecks/ohai/dmi.rb
@@ -1,39 +1,41 @@
-provides "dmi/improve"
-require_plugin("dmi")
-require_plugin("kernel")
+Ohai.plugin(:DmiImproved) do
+  provides "dmi/improve"
+  depends "dmi"
+  depends "kernel"
 
-instruction_set = kernel[:machine].sub('_','-')
-if instruction_set == 'aarch64'
-  dmi[:bios] = {}
-  dmi[:system] = {}
+  instruction_set = kernel[:machine].sub('_','-')
+  if instruction_set == 'aarch64'
+    dmi[:bios] = {}
+    dmi[:system] = {}
 
-  popen4("lshw -quiet -c memory -disable memory") do |pid, stdin, stdout, stderr|
-    stdin.close
-    stdout.each do |line|
-      if line =~ /product/
-        dmi[:bios][:version] = line.chomp.split(": ").last.lstrip
-      end
-      if line =~ /version/
-        dmi[:bios][:release_date] = line.chomp.split(": ").last.lstrip
+    popen4("lshw -quiet -c memory -disable memory") do |pid, stdin, stdout, stderr|
+      stdin.close
+      stdout.each do |line|
+        if line =~ /product/
+          dmi[:bios][:version] = line.chomp.split(": ").last.lstrip
+        end
+        if line =~ /version/
+          dmi[:bios][:release_date] = line.chomp.split(": ").last.lstrip
+        end
       end
     end
-  end
 
-  popen4("lshw -quiet -c system") do |pid, stdin, stdout, stderr|
-    stdin.close
-    stdout.each do |line|
-      if line =~ /vendor/
-        vendor = line.chomp.split(": ").last.lstrip
-        if vendor == 'Hewlett-Packard Development Company, L.P.'
-          vendor = 'HP'
+    popen4("lshw -quiet -c system") do |pid, stdin, stdout, stderr|
+      stdin.close
+      stdout.each do |line|
+        if line =~ /vendor/
+          vendor = line.chomp.split(": ").last.lstrip
+          if vendor == 'Hewlett-Packard Development Company, L.P.'
+            vendor = 'HP'
+          end
+          dmi[:bios][:vendor] = dmi[:system][:manufacturer] = vendor
         end
-        dmi[:bios][:vendor] = dmi[:system][:manufacturer] = vendor
-      end
-      if line =~ /product/
-        dmi[:system][:product_name] = line.chomp.split(": ").last.lstrip
-      end
-      if line =~ /serial/
-        dmi[:system][:serial_number] = line.chomp.split(": ").last.lstrip
+        if line =~ /product/
+          dmi[:system][:product_name] = line.chomp.split(": ").last.lstrip
+        end
+        if line =~ /serial/
+          dmi[:system][:serial_number] = line.chomp.split(": ").last.lstrip
+        end
       end
     end
   end
